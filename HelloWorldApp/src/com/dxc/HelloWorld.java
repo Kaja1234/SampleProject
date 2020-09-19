12244d47b51f9ae44d9bdc3e65794923d5fe00df
kkoppuravuri <kkoppuravuri@192.168.2.2>
2020-09-19 13:27:36 +0530

    added hello world app

---
+++ HelloWorldApp/src/com/dxc/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.dxc;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
