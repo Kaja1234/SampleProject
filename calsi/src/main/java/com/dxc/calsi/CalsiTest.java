d0f2e17ccda887f67c930fa37c9e631f9f0e8c40
kaja1234 <kajakartheek1@gmail.com>
2020-09-21 10:22:15 +0530

    adding calsi

---
+++ calsi/src/main/java/com/dxc/calsi/CalsiTest.java
@@ -0,0 +1,16 @@
+package com.dxc.calsi;
+
+public class CalsiTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = add(4,5);
+		System.out.println(result);
+
+	}
+	
+	public static int add(int n1,int n2) {
+		return n1+n2;
+	}
+
+}
