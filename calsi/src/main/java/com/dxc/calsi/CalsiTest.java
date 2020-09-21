3467c9ac520af00b646f10823f135fc3166dada1
kaja1234 <kajakartheek1@gmail.com>
2020-09-21 10:55:53 +0530

    added subtraction

--- calsi/src/main/java/com/dxc/calsi/CalsiTest.java
+++ calsi/src/main/java/com/dxc/calsi/CalsiTest.java
@@ -6,13 +6,15 @@ public class CalsiTest {
 		// TODO Auto-generated method stub
 		int result = add(4,5);
 		System.out.println(result);
+		int answer = sub(9,7);
+		System.out.println(answer);
 
 	}
 	
 	public static int add(int n1,int n2) {
 		return n1+n2;
 	}
-public static int sub(int  n1,int n2) {
+    public static int sub(int  n1,int n2) {
 		
 		return n1-n2;
 		
